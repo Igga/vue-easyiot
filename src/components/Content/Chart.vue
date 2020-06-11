859b977840d416d51d5fa71db500746d362d6afd
TheIggi <me@paq.su>
2020-06-11 15:42:35 +0300

    'charts'

---
+++ src/components/Content/Chart.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { Line, mixins } from 'vue-chartjs';
+
+const { reactiveProp } = mixins
+
+import { Event } from "@/store/device/types";
+
+@Component({
+    mixins: [reactiveProp, Line]
+})
+export default class Chart extends Vue {
+
+    @Prop() readonly chartData!: Record<string, any>;
+    @Prop() readonly options!: Record<string, any>;
+
+    public renderChart!: (chartData: any, options: any) => void;
+
+    mounted() {
+        this.renderChart(this.chartData, this.options);
+    }
+
+}
+
+</script>
